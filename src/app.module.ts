95ab60490e2969ed9017d057b165c2942bddc80e
러리 <doralife12@naver.com>
2021-12-15 13:22:25 +0900

    Feat: Base filter, middleware, pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import config from './config';
+import { CatchAllFilter } from './filters/catch_all.filter';
+import { HttpExceptionFilter } from './filters/http_exception.filter';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+    TypeOrmModule.forRoot(),
+  ],
+  providers: [
+    { provide: APP_FILTER, useClass: CatchAllFilter },
+    { provide: APP_FILTER, useClass: HttpExceptionFilter },
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
